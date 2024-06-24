d6a2b005611dec9df8874eb11941e17e3606e6be
TheSyg <nlamontanara@gmail.com>
2024-06-24 14:04:22 -0300

    fixed races, still wip tho

--- races.py
+++ races.py
@@ -2,15 +2,15 @@
 
 races = {
     # Alliance
-    'Nelf': 50, 
-    'Dwarf': 50, 
-    'Gnome': 50, 
-    'Human': 50,
-    'Helf': 50,
+    {'Nelf': 50}, 
+    {'Dwarf': 50}, 
+    {'Gnome': 50}, 
+    {'Human': 50},
+    {'Helf': 50},
     # Horde
-    'Troll': 50,
-    'Orc': 50,
-    'Tauren': 50,
-    'Undead': 50,
-    'Goblin': 50
+    {'Troll': 50},
+    {'Orc': 50},
+    {'Tauren': 50},
+    {'Undead': 50},
+    {'Goblin': 50}
 }
