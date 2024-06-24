d87d35746c4aa9217e5d2d1974eb75ff8793f70a
TheSyg <nlamontanara@gmail.com>
2024-06-24 13:30:07 -0300

    Added gear DB WIP

---
+++ gear.py
@@ -0,0 +1,11 @@
+# Gear DB for the sim
+equipment_options = {
+    'helmet': [
+        {'name': 'foo', 'stat': 50},
+        {'name': 'fooddington', 'Agility': 45, 'rAP': 69},
+    ],
+    'necklace': [
+        {'name': 'Prestor\'s Talisman of Connivery', 'Agility': 30},
+        {'name': 'Onyxia Tooth Pendant', 'crit_chance': 1, 'Agility': 12},
+    ],
+}
