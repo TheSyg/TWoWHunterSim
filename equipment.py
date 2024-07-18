4835380732df94ef2666f1776f21d93f144c169a
TheSyg <nlamontanara@gmail.com>
2024-07-18 18:10:27 -0300

    Finished Shoulder slot

--- equipment.py
+++ equipment.py
@@ -1,5 +1,6 @@
 # Gear DB for the sim
-'''Template is: {'name': name, 
+'''
+Template is: {'name': name, 
                 'attack_power': x, 
                 'agility': x, 
                 'crit_chance': x, 
@@ -14,229 +15,348 @@
 '''
 equipment_options = {
     'head': [
-        #to do
+        # to do
     ],
     'neck': [
-        {'name': 'Prestor\'s Talisman of Connivery', 
+        {'name': 'Prestor\'s Talisman of Connivery',
          'attack_power': 0,
-         'agility': 30, 
-         'crit_chance': 0, 
-         'haste': 0, 
-         'hit': 1, 
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         'agility': 30,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 1,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False
-        },
-        ##########
+         },
         {'name': 'Onyxia Tooth Pendant',
          'attack_power': 0,
          'agility': 12,
          'crit_chance': 1,
          'haste': 0,
          'hit': 1,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Mark of Fordring',
          'attack_power': 26,
          'agility': 0,
-         'crit_chance': 1, 
-         'haste': 0, 
+         'crit_chance': 1,
+         'haste': 0,
          'hit': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Stormrage\'s Talisman of Seething',
          'attack_power': 26,
          'agility': 0,
          'crit_chance': 2,
          'haste': 0,
-         'hit':0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Barbed Choker',
          'attack_power': 44,
          'agility': 0,
          'crit_chance': 1,
          'haste': 0,
-         'hit':0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Desert Wind Talisman',
          'attack_power': 0,
          'agility': 15,
          'crit_chance': 1,
          'haste': 1,
          'hit': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'The Eye of Hakkar',
          'attack_power': 40,
          'agility': 0,
          'crit_chance': 1,
          'haste': 0,
          'hit': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Maelstrom\'s Wrath',
          'attack_power': 0,
          'agility': 15,
          'crit_chance': 0,
          'haste': 0,
          'hit': 0,
-         't1': False, 
-         'ZG_set': True, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': True,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Pendant of Celerity',
          'attack_power': 0,
          'agility': 15,
          'crit_chance': 0,
          'haste': 0,
          'hit': 1,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
         {'name': 'Bixxle\'s Necklade of Mastery',
          'attack_power': 0,
          'agility': 0,
          'crit_chance': 1,
          'haste': 1,
          'hit': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False}
-        ##########
     ],
-    'shoulder':[
-        {'name': 'Truestrike Shoulders',
-         'attack_power': 20,
-         'agility': 0,
-         'crit_chance': 0,
-         'haste': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
-         't2': False,
-         'TAQ_set': False,
-         't3': False},
-        ##########
-        {'name': 'Wyrmhide Spaulders', 
+    'shoulder': [
+        {'name': 'Abyssal Leather Shoulders of Striking',
          'attack_power': 0,
-         'agility': 0,
+         'agility': 13,
          'crit_chance': 0,
          'haste': 0,
-         'hit': 2,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         'hit': 1,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
+        
+        {'name': 'Abyssal Mail Pauldrons of Striking',
+         'attack_power': 0,
+         'agility': 13,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 1,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+        
+        {'name': 'Spaulders of the Unseen',
+         'attack_power': 0,
+         'agility': 22,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
         {'name': 'Wyrmtongue Shoulders',
          'attack_power': 0,
          'agility': 23,
          'crit_chance': 0,
          'haste': 0,
          'hit': 0,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False},
-        ##########
+
+        {'name': 'Mantle of Wicked Revenge',
+         'attack_power': 0,
+         'agility': 30,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Truestrike Shoulders',
+         'attack_power': 20,
+         'agility': 0,
+         'crit_chance': 0,
+         'haste': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Wyrmhide Spaulders',
+         'attack_power': 0,
+         'agility': 0,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 2,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
         {'name': 'Shoulderpads of True Flight',
          'attack_power': 0,
          'agility': 18,
          'crit_chance': 0,
          'haste': 0,
          'hit': 1,
-         't1': False, 
-         'ZG_set': False, 
-         'RAQ_set':False,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Giantstalker\'s Epaulets',
+         'attack_power': 0,
+         'agility': 24,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 0,
+         't1': True,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Shoulderguards of the Defiler',
+         'attack_power': 20,
+         'agility': 0,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 2,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Dragonstalker\'s Spaulders',
+         'attack_power': 0,
+         'agility': 23,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 1,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': True,
+         'TAQ_set': False,
+         't3': False},
+
+        {'name': 'Cryptstalker Spaulders',
+         'attack_power': 0,
+         'agility': 29,
+         'crit_chance': 1,
+         'haste': 0,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': False,
+         't3': True},
+
+        {'name': 'Striker\'s Pauldrons',
+         'attack_power': 0,
+         'agility': 26,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 0,
+         't1': False,
+         'ZG_set': False,
+         'RAQ_set': False,
+         't2': False,
+         'TAQ_set': True,
+         't3': False},
+
+        {'name': 'Zandalar Predator\'s Mantle',
+         'attack_power': 0,
+         'agility': 22,
+         'crit_chance': 0,
+         'haste': 0,
+         'hit': 0,
+         't1': False,
+         'ZG_set': True,
+         'RAQ_set': False,
          't2': False,
          'TAQ_set': False,
          't3': False}
-        ##########
     ],
-    'back':[
-        #to do
+    'back': [
+        # to do
     ],
-    'chest':[
-        #to do
+    'chest': [
+        # to do
     ],
-    'hand':[
-        #to do
+    'hand': [
+        # to do
     ],
-    'wrist':[
-        #to do
+    'wrist': [
+        # to do
     ],
-    'waist':[
-        #to do
+    'waist': [
+        # to do
     ],
-    'legs':[
-        #to do
+    'legs': [
+        # to do
     ],
-    'feet':[
-        #to do
+    'feet': [
+        # to do
     ],
-    'finger':[
-        #to do
+    'finger': [
+        # to do
     ],
-    'trinket':[
-        #to do
+    'trinket': [
+        # to do
     ],
-    'melee':[
+    'melee': [
         {'name': 'Fang of the Faceless', 'AP': 28, 'crit_chance': 1, '2h': False},
         {'name': 'Zin\'Rokh, Destroyer of Worlds', 'AP': 72, '2H': True}
     ],
-    'ranged':[
-        #to do
+    'ranged': [
+        # to do
     ],
-    'ammo':[
-        #to do
+    'ammo': [
+        # to do
     ]
 }
