4d50ebc9abb4f6847a76ac7ff9c078022fd976fe
TheSyg <nlamontanara@gmail.com>
2025-02-12 00:35:27 -0300

    WIP

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import random
+import parameters
 
 class Hunter:
     def __init__(self, stats, num_simulations):
-        self.attack_power = stats["attack_power"]
-        self.crit_chance = stats["crit_chance"] / 100 
-        self.hit_chance = stats["hit"] / 100  
+        self.attack_power = parameters.stats["attack_power"]
+        self.crit_chance = parameters.stats["crit_chance"] / 100 
+        self.hit_chance = parameters.stats["hit"] / 100  
         
-        self.min_damage = stats["min_damage"]
-        self.max_damage = stats["max_damage"]
-        self.weapon_speed = stats["weapon_speed"]
-        self.ammo_dps = stats["ammo_dps"]
+        self.min_damage = parameters.stats["min_damage"]
+        self.max_damage = parameters.stats["max_damage"]
+        self.weapon_speed = parameters.stats["weapon_speed"]
+        self.ammo_dps = parameters.stats["ammo_dps"]
 
         self.miss_chance = 0.08 
-        self.trueshot_bonus = 50
+        self.trueshot_bonus = 60
         self.multi_shot_bonus = 172
 
         self.rapid_fire_cd = 300
@@ -98,20 +99,6 @@ class Hunter:
 
         return total_dps / self.num_simulations
 
-
-stats = {
-    "attack_power": 1603,
-    "crit_chance": 30.57,    
-    "hit": 8,              # cap: 8%
-    "min_damage": 585,     
-    "max_damage": 701,     
-    "weapon_speed": 2.99,   
-    "ammo_dps": 17.5         
-}
-
-duration = 60  #PW type fight: 120s
-num_simulations = 69420  # Recommended to do over 50000 sims
-
-hunter = Hunter(stats, num_simulations)
-average_dps = hunter.simulate(duration)
-print(f"Avg DPS in {num_simulations} sims: {round(average_dps, 2)}")
+hunter = Hunter(parameters.stats, parameters.num_simulations)
+average_dps = hunter.simulate(parameters.duration)
+print(f"Avg DPS in {parameters.num_simulations} sims: {round(average_dps, 2)}")
---
+++ parameters.py
@@ -0,0 +1,18 @@
+#################################
+#########Only Modify Here########
+#################################
+stats = {
+    "attack_power": 1603,
+    "crit_chance": 30.57,    
+    "hit": 8,              # cap: 8%
+    "min_damage": 585,     
+    "max_damage": 701,     
+    "weapon_speed": 2.99,   
+    "ammo_dps": 17.5         
+}
+#################################
+#Discord: @syg_##################
+#################################
+
+duration = 60  #PW type fight: 120s
+num_simulations = 69420  # Recommended to do over 50000 sims
